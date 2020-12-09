9aadb8c84fec2f17cdbc77d31ac9852feaa5213a
jesseziros <jtziros2020@gmail.com>
2020-12-08 20:41:44 -0500

    created and designed manifest.json, created and started design of service worker

---
+++ public/service-worker.js
@@ -0,0 +1,40 @@
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/index.js',
+  '/style.css',
+  '../models/transaction.js',
+  'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap'
+];
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
