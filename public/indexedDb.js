cf272c0403a46c5a341b825bad67c52436ef5b43
jesseziros <jtziros2020@gmail.com>
2020-12-15 21:32:04 -0500

    In index.js where fetch fails and saves to indexedDB changed function back to saveRecord(transaction). Refactoring indexedDb.js in progress

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -1,42 +1,32 @@
-const checkForIndexedDb = () => {
-  if (!window.indexedDB) {
-    console.log("This broswer does not support a stable version of IndexedDB")
-    return false
-  }
-  return true;
-}
+let db;
 
-const useIndexedDb = (databaseName, storeName, object, method) => {
-  return new Promise((resolve, reject) => {
-    const request = window.indexedDB.open(databaseName, 1);
-    let
-      db,
-      tx,
-      store;
+const request = indexedDB.open('budget-transaction', 1)
 
-    request.onupgradeneeded = (x) => {
-      const db = request.result;
-      db.createObjectStore(storeName, { keyPath: "_id" });
-    };
-    request.onerror = (x) => {
-      console.log("An error occured");
-    };
-    request.onsuccess = (x) => {
-      db = request.result;
-      tx = db.transaction(storeName, "readwrite");
-      store = tx.objectStore(storeName);
-
-      db.onerror = (x) => console.log("error")
-
-      if (method === "put") {
-        store.put(object);
-      } else if (method === "get") {
-        const all = store.getAll();
-        all.onsuccess = () => resolve(all.result);
-      } else if (method === "delete") {
-        store.delete(object._id);
-      }
-      tx.complete = () => db.close();
-    };
-  });
+request.onupgradeneeded = (event) => {
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
 };
+
+request.onerror = (event) => {
+  console.log("An error occured", event.target.errorCode);
+};
+
+request.onsuccess = (event) => {
+  console.log(event.target.result)
+  db = event.target.result;
+
+  if(navigator.onLine) {
+    addToDatabase();
+  }
+};
+
+//   if (method === "put") {
+//     store.put(object);
+//   } else if (method === "get") {
+//     const all = store.getAll();
+//     all.onsuccess = () => resolve(all.result);
+//   } else if (method === "delete") {
+//     store.delete(object._id);
+//   }
+//   tx.complete = () => db.close();
+// };
